14089ef2aedf42586207d42595b11ea0d16d851b
heldom1proton <heldom1@proton.me>
2023-02-18 11:24:16 +0200

    Set up embedded buttplug server.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use buttplug::client::ButtplugClient;
+use buttplug::core::connector::ButtplugInProcessClientConnectorBuilder;
+use buttplug::server::device::hardware::communication::btleplug::BtlePlugCommunicationManagerBuilder;
+use buttplug::server::ButtplugServerBuilder;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let server = ButtplugServerBuilder::default()
+        .name("Buttboost embedded buttplug server")
+        .comm_manager(BtlePlugCommunicationManagerBuilder::default())
+        .finish()?;
+    let connector = ButtplugInProcessClientConnectorBuilder::default()
+        .server(server)
+        .finish();
+    let client = ButtplugClient::new("Buttboost");
+    client.connect(connector).await?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
